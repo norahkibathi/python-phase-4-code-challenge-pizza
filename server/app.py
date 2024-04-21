0e898960408c01dc6482745ad97fc622a0d2884d
norahkibathi <norah.njeri@student.moringaschool.com>
2024-04-21 21:08:08 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,29 +1,82 @@
-#!/usr/bin/env python3
-from models import db, Restaurant, RestaurantPizza, Pizza
-from flask_migrate import Migrate
-from flask import Flask, request, make_response
-from flask_restful import Api, Resource
-import os
-
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
-
-db.init_app(app)
-
-api = Api(app)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+from werkzeug.urls import url_quote_plus
+from urllib.parse import quote as url_quote
+import app 
 
 
-@app.route("/")
-def index():
-    return "<h1>Code challenge</h1>"
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
 
 
-if __name__ == "__main__":
-    app.run(port=5555, debug=True)
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = "restaurants"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade="all, delete")
+
+    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
+
+    restaurants = association_proxy('restaurant_pizzas', 'pizza', creator=lambda pizza_obj: RestaurantPizza(pizza=pizza_obj))
+
+    def __repr__(self):
+        return f"<Restaurant {self.name}>"
+
+
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = "pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+
+    # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade="all, delete")
+
+    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
+
+    pizzas = association_proxy('restaurant_pizzas', 'restaurant', creator=lambda restaurant_obj: RestaurantPizza(restaurant=restaurant_obj))
+
+    def __repr__(self):
+        return f"<Pizza {self.name}, {self.ingredients}>"
+
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = "restaurant_pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+
+    # add relationships
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+
+    # add serialization rules
+    serialize_rules = ('-pizza.restaurant_pizzas', '-restaurant.restaurant_pizzas')
+
+    # add validation
+    @validates('price')
+    def price_validation(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError("Price must be between 1 and 30")
+        return price
+
+    def __repr__(self):
+        return f"<RestaurantPizza ${self.price}>"
--- server/seed.py
+++ server/seed.py
@@ -13,14 +13,14 @@ with app.app_context():
     RestaurantPizza.query.delete()
 
     print("Creating restaurants...")
-    shack = Restaurant(name="Karen's Pizza Shack", address='address1')
-    bistro = Restaurant(name="Sanjay's Pizza", address='address2')
-    palace = Restaurant(name="Kiki's Pizza", address='address3')
+    shack = Restaurant(name="Kate's Pizza Shack", address='address1')
+    bistro = Restaurant(name="Kennendy's Pizza", address='address2')
+    palace = Restaurant(name="Sandra", address='address3')
     restaurants = [shack, bistro, palace]
 
     print("Creating pizzas...")
 
-    cheese = Pizza(name="Emma", ingredients="Dough, Tomato Sauce, Cheese")
+    cheese = Pizza(name="Wambui", ingredients="Dough, Tomato Sauce, Cheese")
     pepperoni = Pizza(
         name="Geri", ingredients="Dough, Tomato Sauce, Cheese, Pepperoni")
     california = Pizza(
